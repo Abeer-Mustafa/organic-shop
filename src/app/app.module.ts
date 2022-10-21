c60becba36fd0c4da9ff3b648eca767154062281
Abeer Mustafa <abeermustafa925@gmail.com>
2022-10-21 19:56:10 +0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ShoppingCartComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
